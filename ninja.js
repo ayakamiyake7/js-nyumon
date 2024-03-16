6cc6aeaff4a9d0ae63172a174f4bbda8a769cc72
Ayaka Miyake <ayakamiyake7@gmail.com>
2024-03-16 17:01:50 +0900

    - Learn replace

--- ninja.js
+++ ninja.js
@@ -3,4 +3,9 @@
 let windowSize = window.innerWidth;
 // let num1 = 5;
 // let num2 = 8;
-alert(windowSize);
+// alert(windowSize);
+
+let text1 = "リンゴ・バナナ";
+let text2 = "レモン";
+// text1 = text1.replace("リンゴ・バナナ", "リンゴ・");
+// alert((text1 + text2).replace("バナナ", ""));
