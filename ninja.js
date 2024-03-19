35179e7e5f9f3a6423627a8468f7e3582528f3d5
Ayaka Miyake <ayakamiyake7@gmail.com>
2024-03-19 10:00:50 +0900

    Learn 04

--- ninja.js
+++ ninja.js
@@ -9,3 +9,35 @@ let text1 = "リンゴ・バナナ";
 let text2 = "レモン";
 // text1 = text1.replace("リンゴ・バナナ", "リンゴ・");
 // alert((text1 + text2).replace("バナナ", ""));
+
+// const apple = document.querySelector(".apple");
+// const orange = document.querySelector(".orange");
+// const grape = document.querySelector(".grape");
+// apple.addEventListener("click", () => {
+//   alert("apple");
+// });
+// orange.addEventListener("click", () => {
+//   alert("orange");
+// });
+// grape.addEventListener("click", () => {
+//   alert("grape");
+// });
+// console.log(apple);
+// console.log(orange);
+// console.log(grape);
+
+const fruits = document.querySelectorAll("li");
+fruits.forEach((fruit) => {
+  fruit.addEventListener("click", () => alert(fruit.getAttribute("class")));
+});
+
+// 答えの一つ
+// document.addEventListener("DOMContentLoaded", function () {
+//   var listItems = document.querySelectorAll("li");
+
+//   listItems.forEach(function (item) {
+//     item.addEventListener("click", function () {
+//       alert(this.getAttribute("class"));
+//     });
+//   });
+// });
