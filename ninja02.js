27ae4a0d4f77c1d66777cf3800a9e41dbcd4cc9d
Ayaka Miyake <ayakamiyake7@gmail.com>
2024-04-19 07:23:29 +0900

    Learn 3 of advanced

--- ninja02.js
+++ ninja02.js
@@ -2,17 +2,25 @@
 
 // 上級
 // 1
-const anser = document.querySelector(".anser");
-const items = document.querySelectorAll("li");
-let totalWidth = 0;
-items.forEach((item, index) => {
-  if (index != 2) {
-    totalWidth += item.clientWidth;
-  }
-});
-anser.textContent = totalWidth;
+// const anser = document.querySelector(".anser");
+// const items = document.querySelectorAll("li");
+// let totalWidth = 0;
+// items.forEach((item, index) => {
+//   if (index != 2) {
+//     totalWidth += item.clientWidth;
+//   }
+// });
+// anser.textContent = totalWidth;
 
 // 2
-const ninja = [2, 34, 561, 5, 14];
-const futaketa = ninja.filter((item) => 10 <= item && item <= 99);
-console.log(futaketa);
+// const ninja = [2, 34, 561, 5, 14];
+// const futaketa = ninja.filter((item) => 10 <= item && item <= 99);
+// console.log(futaketa);
+
+// 3
+// const ninja = ["5", 6, "3"];
+const ninja = [5, 6, 3];
+const fun = ninja.every((item) => {
+  return typeof item == "number";
+});
+console.log(fun);
