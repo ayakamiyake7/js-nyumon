24fabbc2864c04d699d53c282b8abd8b45c4cf0d
Ayaka Miyake <ayakamiyake7@gmail.com>
2024-05-04 21:40:34 +0900

    Revised 5 of basic

--- ninja03.js
+++ ninja03.js
@@ -1,16 +1,21 @@
 "use strict";
 // 初級
 // 4
-const items = document.querySelectorAll("li");
-items.forEach((item) => {
-  item.addEventListener("click", () => {
-    // const result = item.className;
-    const result = item.getAttribute("class");
-    console.log(result);
-  });
-});
+// const items = document.querySelectorAll("li");
+// items.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     // const result = item.className;
+//     const result = item.getAttribute("class");
+//     console.log(result);
+//   });
+// });
 
 // 5
+const fn = () => {
+  console.log("hi");
+};
+setTimeout(fn, 2000);
+
 // 11
 // 13
 // 16
