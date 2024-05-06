7c25eadb400518b15b34c21a0749a21fb75f67a2
Ayaka Miyake <ayakamiyake7@gmail.com>
2024-05-06 10:30:18 +0900

    Revised 11, 13, 16 of basic

--- ninja03.js
+++ ninja03.js
@@ -11,11 +11,33 @@
 // });
 
 // 5
-const fn = () => {
-  console.log("hi");
-};
-setTimeout(fn, 2000);
+// const fn = () => {
+//   console.log("hi");
+// };
+// setTimeout(fn, 2000);
 
 // 11
+// const target = document.querySelector(".target");
+// const item = document.createElement("p");
+// item.textContent = "ダミーテキストa";
+// console.log(item);
+// target.prepend(item);
+
 // 13
+// const target = document.querySelector(".target");
+// const items = ["ダミーテキストF", "ダミーテキストG"];
+// items.forEach((item) => {
+//   const child = document.createElement("p");
+//   child.textContent = item;
+//   target.append(child);
+// });
+
 // 16
+const trigger = document.querySelector(".trigger");
+const target = document.querySelector(".target");
+trigger.addEventListener("click", () => {
+  target.style.opacity = 1;
+  target.style.transition = "0.4s ease-in";
+});
+
+// basic - 13
