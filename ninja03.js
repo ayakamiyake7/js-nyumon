7197750a4ba446c7e42e3bf9bf5f46409ab06217
Ayaka Miyake <ayakamiyake7@gmail.com>
2024-05-07 12:10:23 +0900

    Revise 1&3 of intermediate

--- ninja03.js
+++ ninja03.js
@@ -33,11 +33,44 @@
 // });
 
 // 16
-const trigger = document.querySelector(".trigger");
-const target = document.querySelector(".target");
-trigger.addEventListener("click", () => {
-  target.style.opacity = 1;
-  target.style.transition = "0.4s ease-in";
-});
+// const trigger = document.querySelector(".trigger");
+// const target = document.querySelector(".target");
+// trigger.addEventListener("click", () => {
+//   target.style.opacity = 1;
+//   target.style.transition = "0.4s ease-in";
+// });
 
 // basic - 13
+
+// 中級
+// 1
+// const box = document.querySelector(".box");
+// const result = box.clientWidth;
+// console.log(result);
+
+// 3
+const boxes = document.querySelectorAll("li");
+let maxHeight = 0;
+boxes.forEach((box) => {
+  console.log(maxHeight);
+  console.log(box.clientHeight);
+  if (maxHeight < box.clientHeight) {
+    maxHeight = box.clientHeight;
+  }
+});
+boxes.forEach((box) => {
+  box.style.height = `${maxHeight}px`;
+});
+console.log(maxHeight);
+
+// 4
+// 7
+// 11
+// 12
+// 13
+// 16
+// 20
+// 21
+// 23
+
+// intermediate - 3
