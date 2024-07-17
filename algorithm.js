533f37a0db7939aaad7a572ad45140633cb66feb
Ayaka Miyake <ayakamiyake7@gmail.com>
2024-07-17 16:08:10 +0900

    Learn babbleSort

--- algorithm.js
+++ algorithm.js
@@ -8,3 +8,26 @@ console.log(GCD(72, 128));
 
 // バブルソート
 // https://sevendays-study.com/algorithm/day5_bubble_c.html
+const MAX_LENGTH = 5;
+function showData(array) {
+  console.log(array.join(" "));
+}
+
+function babbleSort(array) {
+  let length = array.length;
+  let tmp;
+  for (let i = 0; i < MAX_LENGTH - 1; i++) {
+    showData(array);
+    for (let j = i + 1; j < MAX_LENGTH; j++) {
+      if (array[i] < array[j]) {
+        // i=0 and j=1: array[0] < array[1] = 5<2
+        tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+      }
+    }
+  }
+  showData(array);
+}
+let array = [5, 2, 3, 1, 4];
+babbleSort(array);
